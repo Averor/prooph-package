cdaa4cc32e3d6778045558fa98780d35b9d7526b
Averor <averor.dev@gmail.com>
2017-11-24 15:34:41 +0100

    Abstract ReadModelProjector instead of poorly designed factory

---
+++ src/AveProophPackage/Projection/ReadModelProjector.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AveProophPackage\Projection;
+
+use Prooph\EventStore\Projection\ProjectionManager;
+use Prooph\EventStore\Projection\ReadModel;
+use Prooph\EventStore\Projection\Projector;
+use Prooph\EventStore\Projection\ReadModelProjector as IReadModelProjector;
+
+/**
+ * Class ReadModelProjector
+ *
+ * @package AveProophPackage\Projection
+ * @author Averor <averor.dev@gmail.com>
+ */
+abstract class ReadModelProjector
+{
+    /** @var string */
+    protected static $projectorName = 'Undefined';
+
+    /** @var IReadModelProjector */
+    protected $projector;
+
+    /**
+     * @param ProjectionManager $projectionManager
+     * @param ReadModel $readModel
+     */
+    public function __construct(ProjectionManager $projectionManager, ReadModel $readModel)
+    {
+        /** @var IReadModelProjector $projector */
+        $this->projector = $projectionManager->createReadModelProjection(
+            static::$projectorName,
+            $readModel,
+            [
+                Projector::OPTION_PCNTL_DISPATCH => true
+            ]
+        );
+    }
+
+    /**
+     * @return IReadModelProjector
+     */
+    abstract public function project() : IReadModelProjector;
+}
